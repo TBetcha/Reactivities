f3fa1e067bd510770a8bc76da3da02791a842c2b
Tbetcha <troy.boettger@gmail.com>
2025-12-26 15:00:37 -0500

    some db stuff and other setup

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d38c6694dcaf9a258a247ed055ad09bc07aad54a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e2876d08a810d59fadc74755c7f1bd8c8598800")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
