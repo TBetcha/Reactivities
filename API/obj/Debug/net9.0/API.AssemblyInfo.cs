7f2ec58b563b3d87be28567be3c0afb7f616324a
Tbetcha <troy.boettger@gmail.com>
2025-12-29 17:50:24 -0500

    frontend stuff

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3fa1e067bd510770a8bc76da3da02791a842c2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3738eeb4a501c2da3bae9d4ad31269ec690665b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
