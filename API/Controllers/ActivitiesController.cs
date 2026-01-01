dfccee4c12507c2230cf915a63b7a44e6e321fc6
Tbetcha <troy.boettger@gmail.com>
2026-01-01 14:01:40 -0500

    new command and edit controller

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using Application.Activities.Commands;
 using Application.Activities.Queries;
 using Domain;
 using MediatR;
@@ -6,18 +7,31 @@ using Persistence;
 
 namespace API.Controllers
 {
-    public class ActivitiesController(AppDbContext context, IMediator mediator) : BaseApiController
+    public class ActivitiesController(AppDbContext context) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await mediator.Send(new Application.Activities.Queries.GetActivityList.Query());
+            return await Mediator.Send(new GetActivityList.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivityDetail(string id)
         {
-            return await mediator.Send(new GetActivityDetails.Query { Id = id });
+            return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> CreateActivity(Activity activity)
+        {
+            return await Mediator.Send(new CreateActivity.Command { Activity = activity });
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditActivity(Activity activity)
+        {
+            await Mediator.Send(new EditActivity.Command { Activity = activity });
+            return NoContent();
         }
     }
 }
--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f2ec58b563b3d87be28567be3c0afb7f616324a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a2ed94e817a64940a58d1dd5d51e21420ffb7d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
