ca7f9bf19769fa7a885c1649444910662cf2f0de
Tbetcha <troy.boettger@gmail.com>
2026-01-21 13:02:06 -0500

    remove fresh loading prior to entering form

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f57359a3c597cebafc88b17f77e6fd26face276")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+110e9e4450dec8e0f1eb33cb06b41a550bd884d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
